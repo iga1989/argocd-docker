1d287a1601740a76fb3b39064bcb6f10fc425d10
iga1989 <issetimba@gmail.com>
2024-07-13 17:30:09 -0500

    first commit

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'ARGOCD IS AWESOME !'
+    return 'NYANGE NAOMI NABUKEERA !'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
